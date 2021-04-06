5b3620699ad3d83fc6160232a16d5444f82e2b3d
Michael Jordan <michdan25@gmail.com>
2021-04-06 20:35:13 +1200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Michael Jordan
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            Cursor.lockState = CursorLockMode.Confined; 
+        }
+        else
+        {
+            Debug.LogError("Second Instance of GameManager was created, this instance was destroyed.");
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    #endregion
+
+    //Volume Settings
+    public static float MasterVolume { get; set; } = 1.0f;
+    public static float SoundEffectVolume { get; set; } = 1.0f;
+    public static float BackGroundVolume { get; set; } = 1.0f;
+
+    public double GameTime = 0.0;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameTime += Time.deltaTime;
+    }
+}
