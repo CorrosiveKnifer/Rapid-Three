97d2e81c8c4a009e80261d16a589877061332ce3
Michael Jordan <michdan25@gmail.com>
2021-04-09 17:42:33 +1200

    my camera

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -38,6 +38,11 @@ public class Boulder : MonoBehaviour
         indicatorPrefab.SetActive(false);
     }
 
+    private void FixedUpdate()
+    {
+        GroundedUpdate();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -57,7 +62,7 @@ public class Boulder : MonoBehaviour
         screenPoint.z = 10.0f; //distance of the plane from the camera
         playerLook = Camera.main.ScreenToWorldPoint(screenPoint);
 
-        GroundedUpdate();
+        
 
         GetRayDrawWhenAim();
         if (playercontro.m_bIsLifting)
@@ -135,8 +140,8 @@ public class Boulder : MonoBehaviour
                 {
                     float distanceFromPlayer = Mathf.Clamp(1.0f - (Vector2.Distance(playercontro.playerSprite.transform.position, transform.position) / maximumShakeDistance), 0.01f, 1.0f);
 
-                    float fCamShake = (Mathf.Abs(projectileRB.velocity.y) / 1000) * distanceFromPlayer;
-                    CameraController.instance.StartShake(fCamShake, 1.0f);
+                    float fCamShake = (Mathf.Abs(projectileRB.velocity.y) / 100) * distanceFromPlayer;
+                    CameraController.instance.StartShake(fCamShake, 0.3f);
                 }
                 break;
             }
