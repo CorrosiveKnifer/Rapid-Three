62b9253a92430a5f464a0985711c77af11a973ee
William <55791230+GrizzBe@users.noreply.github.com>
2021-04-15 18:41:06 +1200

    Modified level, slowed carry run anim, tuned lifting

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Rachael work, William de Beer
+/// Rachael Colaco, William de Beer
 /// </summary>
 /// 
 public class Boulder : MonoBehaviour
